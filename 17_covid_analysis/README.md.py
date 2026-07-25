31083f26e1ddbcbce4bb9a90a32f614f574c488b
LENOVO" Andrew <ap10222006@gmail.com>
2026-07-25 15:45:42 -0600

    Add COVID-19 analysis with real global data and visualizations.

---
+++ 17_covid_analysis/README.md.py
@@ -0,0 +1,21 @@
+# COVID-19 Data Analysis 🦠
+
+## Description
+Analysis of real COVID-19 data across 5 major countries
+examining cases, deaths, vaccinations and cases per million.
+
+## Tech Stack
+- Python Pandas, Matplotlib
+- Jupyter Notebook
+- Real OWID COVID dataset
+
+## Key Findings
+- USA led in total cases and deaths
+- India led in total vaccinations population effect
+- UK had highest cases per million fairer comparison
+
+## Data Source
+Our World in Data OWID COVID-19 dataset
+
+## How to run
+Open covid_analysis.ipynb in Jupyter
