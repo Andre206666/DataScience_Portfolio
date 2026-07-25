8ae3923a85ddfeebdec1eab242b5af5c93d4c748
LENOVO" Andrew <ap10222006@gmail.com>
2026-07-25 15:11:26 -0600

    Add Sentiment Analysis NLP project with 95% accuracy

---
+++ 16_sentiment_analysis/README.py
@@ -0,0 +1,26 @@
+
+# Twitter Sentiment Analysis 📝
+
+## Description
+NLP project analyzing 31,962 tweets to determine sentiment
+using TextBlob and Machine Learning.
+
+## Tech Stack
+- Python Pandas, NLTK, TextBlob
+- Scikit-learn Logistic Regression, TF-IDF
+- Jupyter Notebook
+
+## Key Findings
+- 50% positive, 35% neutral, 15% negative tweets
+- Most common positive words: love, happy, good
+- Most common negative words: sad, hate, polar bear
+- Unexpected finding: environmental concerns polar bears
+  dominate negative tweets
+
+## Machine Learning
+- Model: Logistic Regression + TF-IDF vectorizer
+- Accuracy: 95%
+- Limitation: bias toward positive due to class imbalance
+
+## How to run
+Open sentiment_analysis.ipynb in Jupyter
